6e09c758bda1f41153de6f908cdd46bb310d88a5
Satoru Fujimori <satoru.fujimori@gmail.com>
2017-08-27 19:45:53 +0900

    Add Sub.storyboard

--- SwitchWindowSample/AppDelegate.cs
+++ SwitchWindowSample/AppDelegate.cs
@@ -12,7 +12,18 @@ namespace SwitchWindowSample
 
         public override void DidFinishLaunching(NSNotification notification)
         {
-            // Insert code here to initialize your application
+            // 1. ファイル→新しいファイル→ストーリーボードでSub.storyboardを作成
+            // 2. 作成したStoryboardをIBで開いてWindowControllerを追加
+            // 3. IBでWindowControllerを選択→IdentityInspectorを選択→StoryboardIDにMainWindowと入力
+
+            // StoryBoardを呼び出し
+            var storyboard = NSStoryboard.FromName("Sub", null);
+
+            // StoryBoardからViewControllerを呼び出し
+            var controller = storyboard.InstantiateControllerWithIdentifier("MainWindow") as NSWindowController;
+
+            // 表示
+            controller.ShowWindow(this);
         }
 
         public override void WillTerminate(NSNotification notification)
